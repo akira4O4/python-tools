260bb9dc4580ee18876d230a9a5f99867a034057
Akira.404 <akira.404@outlook.com>
2024-08-01 15:59:22 +0800

    Update

--- tools/random_sampling.py
+++ tools/random_sampling.py
@@ -5,9 +5,10 @@ import shutil
 from tqdm import tqdm
 
 if __name__ == '__main__':
-    random_rate = 10000
-    root = r''
-    output = r''
+    random_rate = 128
+    mode = 'copy'
+    root = r'D:\llf\dataset\dog_cat\all'
+    output = r'D:\llf\dataset\dog_cat\128'
 
     images = get_images(root)
     images_len = len(images)
@@ -17,9 +18,14 @@ if __name__ == '__main__':
         new_data = sample(images, random_rate)
 
     for image in tqdm(new_data):
+
         image_basename = os.path.basename(image)
         new_image_path = image.replace(root, output)
         new_image_dir = new_image_path.split(image_basename)[0]
+
         if not os.path.exists(new_image_dir):
             os.makedirs(new_image_dir, exist_ok=True)
-        shutil.copy(image, new_image_dir)
+        if mode.lower() == 'copy':
+            shutil.copy(image, new_image_dir)
+        elif mode.lower() == 'move':
+            shutil.move(image, new_image_dir)
